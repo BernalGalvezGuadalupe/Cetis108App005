6c8116505e3448a14fec13b6ddd2e3eb9d085c61
BernalGalvezGuadalupe <15325061080270@cetis108.edu.mx>
2017-06-14 18:57:39 -0700

    Initial commit

---
+++ app/src/main/java/com/example/gualberto/prueba/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.gualberto.prueba;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button nuevo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void mostrar(View v) {
+        Intent nuevo = new Intent(this, Tema_tipo_Activity.class);
+        startActivity(nuevo);
+
+    }
+
+}
+
+
+
+
+
